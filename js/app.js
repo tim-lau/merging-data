c75b8059527c5f0224674b17f1f03960d245ea22
timothyl@uw.edu <Tim@d-128-208-14-113.dhcp4.washington.edu>
2014-11-10 17:27:44 -0800

    Added filters, ordering, button states

--- js/app.js
+++ js/app.js
@@ -5,3 +5,23 @@
     Add code here to create a new Angular application and controller.
     The array of employee objects is already in a global variable named 'pawneeEmployees'
 */
+
+angular.module('AddressBook', [])
+	.controller('AddressController', function($scope) {
+		//intialize variables with list of movies so that it'll be accessed in the view
+		$scope.employees = pawneeEmployees;
+		$scope.filteredEmployees = pawneeEmployees;
+
+		$scope.sortCol = 'lastName';
+		$scope.employeeSearch = undefined;
+
+		$scope.sortBy = function(sortCol) {
+            $scope.sortCol = sortCol;
+        };
+
+        $scope.isSortedBy = function(colName) {
+            return $scope.sortCol == colName;
+
+		};
+
+	});
